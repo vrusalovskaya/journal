83c05e0a23d1d9131fcf4680b4ad01ff7dbd5539
victoria_rusalovskaya <vika.rusalovskaya@gmail.com>
2024-09-22 20:47:09 +0400

    add health servlet

---
+++ src/main/java/org/example/presentation/servlets/HealthServlet.java
@@ -0,0 +1,19 @@
+package org.example.presentation.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/health")
+public class HealthServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().write("OK");
+    }
+}
